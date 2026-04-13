9daee27be1b8717738e6e828ebf5ed72034eb748
qyd20200101 <1326255779@qq.com>
2026-04-13 22:29:57 +0800

    学习日报

--- my-sass-project/src/main.ts
+++ my-sass-project/src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from "./router";
 import './router/guard';//关键：导入路由守卫
 import { createPinia } from 'pinia';
-
+import  "./test";
 const app = createApp(App);
 
 // 在实例app上调用插件
